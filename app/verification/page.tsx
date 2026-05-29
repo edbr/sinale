a267aabdfcf94450dd53768147b39848429b79cb
Eduardo Ribeiro <ribeiro.ebb@gmail.com>
2026-05-29 13:09:29 -0700

    another verification

---
+++ app/verification/page.tsx
@@ -0,0 +1,10 @@
+export default function VerificationPage() {
+  return (
+    <main className="container mx-auto py-20">
+      <p>
+        Impact-Site-Verification:
+        1321ef41-cba2-4dc3-8aec-58d8baf072d1
+      </p>
+    </main>
+  )
+}
